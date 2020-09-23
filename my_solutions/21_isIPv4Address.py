8cd7255ea39d76f370c25befb6712a19c89c83df
Sikder Tahsin Al-Amin <sal-amin2@uh.edu>
2020-09-23 13:35:59 -0500

    Update 21_isIPv4Address.py

--- my_solutions/21_isIPv4Address.py
+++ my_solutions/21_isIPv4Address.py
@@ -1,7 +1,8 @@
 """
 Author: Sikder Tahsin Al Amin
 Problem:
-An IP address is a numerical label assigned to each device (e.g., computer, printer) participating in a computer network that uses the Internet Protocol for communication. There are two versions of the Internet protocol, and thus two versions of addresses. One of them is the IPv4 address.
+An IP address is a numerical label assigned to each device (e.g., computer, printer) participating in a computer network that uses the Internet Protocol for communication. 
+There are two versions of the Internet protocol, and thus two versions of addresses. One of them is the IPv4 address.
 Given a string, find out if it satisfies the IPv4 address naming rules.
 
 Example
